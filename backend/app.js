29472065939c0b894ed5a2673761954e6be01271
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-09-08 01:31:01 +0530

    backend

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
+import router from "./routes/user.routes.js"
+import fileUpload from 'express-fileupload';
+
+const app = express();
+
+dotenv.config()
+
+app.use(cors());
+
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:'/temp/',
+}));
+
+app.use("/api/user",router);
+
+export default app
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import app from './app.js';
+import dbConnect from './database/dbConnection.js';
+import cloudinary from 'cloudinary';
+
+
+cloudinary.v2.config({
+    cloud_name:process.env.CLOUDINARY_CLIENT_NAME,
+    api_key:process.env.CLOUDINARY_CLIENT_API,
+    api_secret:process.env.CLOUDINARY_CLIENT_SECRET
+})
+
+
+
+
+const port = process.env.PORT || 8000;
+
+
+dbConnect().then(()=>{
+    app.listen(port,()=>{
+        console.log(`app listening on ${port}`);
+    })
+}).catch(err=>{
+    console.log(`error in server.js in dbConect ${err}`);
+})
