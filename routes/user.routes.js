056bf52273a5c2ade1f2bb81e4f6c2f4f437f03f
Pradyuman Sharma <pradyumanmusic@gmail.com>
2024-09-08 09:00:33 +0530

    Push backend

---
+++ routes/user.routes.js
@@ -0,0 +1,14 @@
+import express  from "express";
+import {feedBack, imgDelete} from '../controllers/feedbak.controller.js'
+import geminiRes from "../utils/gemini.js";
+
+
+
+const router = express.Router();
+
+
+router.post('/feedback',feedBack);
+router.post('/geminires',geminiRes);
+
+
+export default router;
